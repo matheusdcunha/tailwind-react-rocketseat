3a71d60c3fc6db06b82939c5315ddf79a1d0b80d
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2025-03-22 10:42:48 -0300

    Adicionar Dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
+import { ManagerRoutes } from "./ManagerRoutes";
 
 export function Routes() {
   return (
     <BrowserRouter>
       {/* <AuthRoutes /> */}
-      <EmployeeRoutes />
+      {/* <EmployeeRoutes /> */}
+      <ManagerRoutes/>
     </BrowserRouter>
   )
 }
